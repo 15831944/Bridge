5e1778d81ddd1519c70d1fa11bb5615d1a971f1c
Ohto-Ai <zhu.thatboy@outlook.com>
2021-07-30 23:29:21 +0800

    添加TcpOutputBridge[todo]

---
+++ Bridge/TcpOutputBridge.cpp
@@ -0,0 +1,37 @@
+﻿#include <QTcpSocket>
+#include <QTcpServer>
+#include <QCoreApplication>
+#include "TcpOutputBridge.h"
+
+bool TcpOutputBridge::init(json argPackage)
+{
+	try
+	{
+		// todo: Insert init bridge code here.
+		return true;
+	}
+	catch (...)
+	{
+	}
+	return false;
+}
+
+void TcpOutputBridge::uninit()
+{
+	// todo: Insert uninit code here.
+}
+
+void TcpOutputBridge::setConfig(json setting)
+{
+	// todo: Insert config code here.
+}
+
+BridgeIOBase::json TcpOutputBridge::config() const
+{
+	// todo: Insert get config code here.
+}
+
+void TcpOutputBridge::write(QByteArray data)
+{
+	// todo: Insert write code here.
+}
---
+++ Bridge/TcpOutputBridge.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+#include "BridgeOutputBase.h"
+class TcpOutputBridge final :
+	public BridgeOutputBase
+{
+public:
+	/**
+	 * @brief 打开接口
+	 * @param argPackage 含有filename参数包
+	 * @return 是否成功
+	*/
+	bool init(json argPackage) override;
+
+	/**
+	 * @brief 关闭接口
+	*/
+	void uninit() override;
+
+	/**
+	 * @brief 添加配置信息
+	 * @param setting 含有mode<overwrite/skip/block>的参数包
+	*/
+	void setConfig(json setting) override;
+
+	/**
+	 * @brief 获取配置信息
+	 * @return 配置信息
+	*/
+	json config() const override;
+	
+	/**
+	 * @brief 写入文件
+	 * @param data 待写入数据
+	*/
+	void write(QByteArray data) override;
+protected:
+};
